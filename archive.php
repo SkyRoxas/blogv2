198741535bf02920636e12a0b83b3a7a43ee5e27
jimmy8646 <yujiechueh@gmail.com>
2017-05-20 01:30:39 +0800

    add archive tpl #1

--- archive.php
+++ archive.php
@@ -1,2 +1,12 @@
 <?php  get_header(); ?>
+<div id ="banner" class ="region">
+  <?php get_template_part('tpl-components/imageblock_01') ?>
+</div>
+<div id ="page" class ="region">
+  <div id = "content">
+    <?php if(have_posts()) :?>
+      <?php get_template_part('tpl-components/archive_01') ?>
+    <?php endif; ?>
+  </div>
+</div>
 <?php  get_footer(); ?>
