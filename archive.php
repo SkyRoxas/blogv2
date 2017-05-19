a71dce2544f9d66f865389491ae0d15efa8e859b
jimmy8646 <yujiechueh@gmail.com>
2017-05-20 04:23:51 +0800

    need some sleep

--- archive.php
+++ archive.php
@@ -4,6 +4,14 @@
 </div>
 <div id ="page" class ="region">
   <div id = "content">
+      <div id="widgetized-area">
+    <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('bonze-widget')) : else : ?>
+        <div class="pre-widget">
+            <p><strong>棒子小工具區塊</strong></p>
+            <p>你如果看到這個可能怪怪的喔</p>
+        </div>
+    <?php endif; ?>
+    </div>
     <?php if(have_posts()) :?>
       <?php get_template_part('tpl-components/archive_01') ?>
     <?php endif; ?>
--- functions.php
+++ functions.php
@@ -21,4 +21,53 @@ function add_scripts() {
 
   add_action( 'wp_enqueue_scripts', 'add_scripts' );
 
+/**
+* Register our sidebars and widgetized areas.
+*
+*/
+function bonze_widgets_init() {
+    if (function_exists('register_sidebar')) {
+        register_sidebar(array(
+            'name' => '棒子小工具區塊',
+            'id'   => 'bonze-widget',
+            'description'   => '不然你想看到啥',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h4>',
+            'after_title'   => '</h4>'
+        ));
+    }
+}
+
+add_action( 'widgets_init', 'bonze_widgets_init' );
+
+//註冊bonze分類區塊
+function register_bonze_archive_widget() {
+    register_widget( 'Bonze_Archive_Widget' );
+}
+
+add_action( 'widgets_init', 'register_bonze_archive_widget' );
+
+class Bonze_Archive_Widget extends WP_Widget {
+    function __construct() {
+        $widget_options = array(
+            'classname' => 'bonze_taxonomy_widget',
+            'description' => '棒子分類小工具',
+        );
+        parent::__construct( 'bonze_taxonomy_widget', '棒子超屌分類', $widget_options );
+    }
+
+    public function widget( $args, $instance ) {
+        wp_list_categories($args='');
+    }
+
+    public function form( $instance ) {
+        wp_list_categories(array(
+            'orderby'=>'modified',
+            'order'=>'DESC',
+            'show_count'=>true,
+            'title_li'=>'',
+        ));
+    }
+}
 ?>
