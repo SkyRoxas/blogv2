9e4a331ae85eb3029b37c906a9459163ac13b71f
skyroxas <skyroxas1521@gmail.com>
2017-05-21 14:00:03 +0800

    clean code

--- tpl-components/categories-block.php
+++ tpl-components/categories-block.php
@@ -1,31 +1,23 @@
 <div class ="categories-block">
   <ul>
     <?php
-          $args = array(
+          $categories = get_categories( array(
             'orderby' => 'name',
             'show_count' => true,
             'parent' => 0
-          );
-
-
-          $categories = get_categories( $args );
-
+          ));
 
           foreach ( $categories as $category ) {
-            echo '<li>
-                    <a href="' . get_category_link( $category->term_id ) . '" >' . $category->name . '</a>
-                  </li>';
-            $sub_args = array(
+            echo '<li><a href="' . get_category_link( $category->term_id ) . '" >' . $category->name . '</a></li>';
+
+            $sub_categories = get_categories(array(
                     'orderby' => 'name',
                     'show_count' => true,
                     'parent' => $category->term_id,
-                  );
-            $sub_categories = get_categories($sub_args);
+                  ));
 
             foreach ( $sub_categories as $sub_category ) {
-                    echo '<li>
-                            <a href="' . get_category_link( $sub_category->term_id ) . '">' . $sub_category->name . '</a>
-                          </li>';
+                    echo '<li><a href="' . get_category_link( $sub_category->term_id ) . '">' . $sub_category->name . '</a></li>';
                   }
           }
     ?>
