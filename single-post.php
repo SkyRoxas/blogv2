6cb80900bd939fead5f14bde18e98af5ac105c60
skyroxas <skyroxas1521@gmail.com>
2017-05-24 04:23:18 +0800

    single layout style

--- single-post.php
+++ single-post.php
@@ -2,14 +2,12 @@
 <div id ="banner" class ="region">
   <?php get_template_part('tpl-components/imageblock_01') ?>
 </div>
-<div id ="page" class ="region">
-  <div id = "content">
-      <?php
-if ( function_exists('yoast_breadcrumb') ) {
-yoast_breadcrumb('<div id="breadcrumbs">','</div>');
-}
-?>
-      <?php the_content(); ?>
+<div id ="page" class ="region p-lg-5 p-md-5 p-sm-5 p-2">
+  <div id = "content" class ="container p-lg-5 p-md-5 p-sm-5 p-4" style ="background:#ffffff;">
+      <?php get_template_part('tpl-components/breadcrumb') ?>
+      <article>
+        <?php the_content(); ?>
+      </article>
   </div>
 </div>
 <div id="bottom">
