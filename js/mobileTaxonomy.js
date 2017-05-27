7a2d0fddf6b95e598fb9df2715a9c72111f65dd8
skyroxas <skyroxas1521@gmail.com>
2017-05-27 20:59:53 +0800

    阿J bug

--- js/mobileTaxonomy.js
+++ js/mobileTaxonomy.js
@@ -1,27 +1,28 @@
-(function($) {
-    $(document).ready(function() {
-        var windoWidth = $(window).width()
-        if (windoWidth <= 576) {
-            $('#bottom .categories-block').addClass('mobile-hide')
-        } else if (windoWidth > 576) {
-            $('#bottom .categories-block').removeClass('mobile-hide')
-        }
-        //<---resize--->
-        $(window).resize(function() {
-            var resizeWidth = $(window).width()
-            if (resizeWidth <= 576) {
-                $('#bottom .categories-block').addClass('responsive')
-            } else if (resizeWidth > 576) {
-                $('#bottom .categories-block').removeClass('responsive')
-            }
-        })
-        //<---resize 結束--->
-        $('.header-nav').click(function() {
-            var windoWidth = $(window).width()
-            if (windoWidth <= 576) {
-                $('#bottom .categories-block').toggleClass('mobile-hide')
-                $('#bottom .categories-block').css('position', 'fixed','z-index','9999')
-            }
-        })
+(function ($) {
+
+  $(document).ready(function () {
+    var windoWidth = $(window).width()
+    if (windoWidth <= 576) {
+      $('#bottom .categories-block').addClass('mobile-hide')
+    } else if (windoWidth > 576) {
+      $('#bottom .categories-block').removeClass('mobile-hide')
+    }
+        // <---resize--->
+    $(window).resize(function () {
+      var resizeWidth = $(window).width()
+      if (resizeWidth <= 576) {
+        $('#bottom .categories-block').addClass('mobile-hide')
+      } else if (resizeWidth > 576) {
+        $('#bottom .categories-block').removeClass('mobile-hide')
+      }
     })
+        // <---resize 結束--->
+    $('.header-nav').click(function () {
+      var windoWidth = $(window).width()
+      if (windoWidth <= 576) {
+        $('#bottom .categories-block').toggleClass('mobile-hide')
+        $('#bottom .categories-block').css('position', 'fixed', 'z-index', '9999')
+      }
+    })
+  })
 })(jQuery)
