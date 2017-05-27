e2c190e5def3e66f97da119ecd7db434aa8357d9
skyroxas <skyroxas1521@gmail.com>
2017-05-27 20:16:14 +0800

    ajax loading

--- js/ajaxLoop.js
+++ js/ajaxLoop.js
@@ -1,9 +1,9 @@
 // ajax
 jQuery(function ($) {
-  var page = 1
+  var page = 2
   var loading = true
   var $window = $(window)
-  var $content = $('body.blog #content')
+  var $content = $('.ajax-bottom')
   var load_posts = function () {
     $.ajax({
       type: 'GET',
@@ -13,13 +13,33 @@ jQuery(function ($) {
       beforeSend: function () {
       },
       success: function (data) {
-        //alert('success')
-        console.log(data)
+        // alert('success')
+        // console.log(data)
+        $content.before(data)
       },
       error: function (jqXHR, textStatus, errorThrown) {
-        alert(jqXHR + ' :: ' + textStatus + ' :: ' + errorThrown)
+        console(jqXHR + ' :: ' + textStatus + ' :: ' + errorThrown)
       }
     })
   }
-  load_posts()
+
+  $(document).ready(function () {
+    $window.scroll(function () {
+      var content_offset = $content.offset()
+      var content_position = $content.position()
+      if ($(window).scrollTop() > $content.offset().top - $(window).height() && $content.parent().scrollLeft() > content_position.left - $content.parent().width()) {
+        loading = true
+        page++
+        load_posts()
+      }
+    })
+
+    $content.parent().scroll(function () {
+      if ($content.parent().scrollLeft() > $content.position().left - $content.parent().width()) {
+        loading = true
+        page++
+        load_posts()
+      }
+    })
+  })
 })
