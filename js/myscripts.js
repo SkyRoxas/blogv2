089e31f4f7fda2539b5b085a8cb9071d9679c877
skyroxas <skyroxas1521@gmail.com>
2017-07-06 18:27:21 +0800

    chart develop

--- js/myscripts.js
+++ js/myscripts.js
@@ -20,21 +20,48 @@
     }
   }
 
-  var json = [
+  var jsonData = [
     {
       'id': 1,
       'title': 'Html',
-      'count': 20
+      'count': 40
     }, {
       'id': 2,
       'title': 'Css',
-      'count': 10
+      'count': 140
     }, {
       'id': 3,
       'title': 'JavaScript',
-      'count': 50
+      'count': 90
+    }, {
+      'id': 4,
+      'title': 'JavaScript',
+      'count': 90
+    }, {
+      'id': 5,
+      'title': 'test',
+      'count': 90
     }
+
   ]
+
+  myChart.data = {
+    json: jsonData,
+    getTotal: function () {
+      var total = 0
+      for (var i = 0; i < myChart.data.json.length; i++) {
+        total = total + myChart.data.json[i].count
+      }
+      return total
+    },
+    getData: function () {
+      for (var i = 0; i < myChart.data.json.length; i++) {
+        var angle = (myChart.data.json[i].count / myChart.data.getTotal()) * 360
+        myChart.data.json[i]['angle'] = angle
+      }
+      return myChart.data.json
+    }
+  }
 
 /**
  * Svg 參數
@@ -43,9 +70,19 @@
  */
   myChart.svg = defaults
 
+/**
+ * svg 方法
+ * @class svg_method
+ */
   myChart.svgMethod = {
-    path: function () {
-      return '<path class = ' + $class + ' style ="fill: ' + $fill + ' stroke: ' + $stroke + 'stroke-width:"' + $strokeWidth + '>'
+    /**
+     * [description]
+     * @param  {string} $class svg path attributes class
+     * @param  {string} $d     svg path attributes d
+     * @return {string}        svg path html
+     */
+    path: function ($class, $d) {
+      return '<path class="' + $class + '" style="fill:#fe0; stroke:#fff; stroke-width:3;" d = "' + $d + '"/>'
     }
   }
 
@@ -56,34 +93,34 @@
  */
   myChart.math_method = {
 
-/**
- * 計算 x 軸終點座標
- *
- * @method target_X
- * @param {number} $angle
- * @return {number} [description]
- */
+    /**
+     * 計算 x 軸終點座標
+     *
+     * @method target_X
+     * @param {number} $angle
+     * @return {number} [description]
+     */
     coordinate_X: function ($angle) {
       return myChart.svg.move.x + myChart.svg.radius.x * Math.cos($angle * Math.PI / 180)
     },
 
-/**
- * 計算 y 軸終點座標
- *
- * @method target_Y
- * @param  {number} $angle [description]
- * @return {number}      [description]
- */
+    /**
+     * 計算 y 軸終點座標
+     *
+     * @method target_Y
+     * @param  {number} $angle [description]
+     * @return {number}      [description]
+     */
     coordinate_Y: function ($angle) {
       return myChart.svg.move.y - myChart.svg.radius.y * Math.sin($angle * Math.PI / 180)
     },
 
-/**
- * 1 為大角度弧線，0 為小角度弧線
- * @method flags
- * @param  {number} $angle [description]
- * @return {number}        [description]
- */
+    /**
+     * 1 為大角度弧線，0 為小角度弧線
+     * @method flags
+     * @param  {number} $angle [description]
+     * @return {number}        [description]
+     */
     flags: function ($angle) {
       if ($angle > 180) {
         return 1
@@ -117,26 +154,37 @@
     this.target_Y = myChart.math_method.coordinate_Y($angle + $startangle)
   }
 
-  var k = new myChart.ChartPart(90)
-  var l = new myChart.ChartPart(190, k.angle)
-  var m = new myChart.ChartPart(60, (k.angle + l.angle))
-  var n = new myChart.ChartPart(20, (k.angle + l.angle + m.angle))
+  myChart.createChart = {
+    pathItem: function (starangle = 0, svgPath = '') {
+      for (var i = 0; i < myChart.data.getData().length; i++) {
+        var part = new myChart.ChartPart(myChart.data.getData()[i].angle, starangle)
+        var d = 'M100 100,L ' + part.starting_X + ' ' + part.starting_Y + 'A 100 100 0 ' + myChart.math_method.flags(myChart.data.getData()[i].angle) + ' 0' + part.target_X + ' ' + part.target_Y + ',Z'
+        svgPath = svgPath + ' ' + myChart.svgMethod.path('part' + i, d)
+        starangle += myChart.data.getData()[i].angle
+      }
+      return svgPath
+    },
+    beforeItem: function (svgElement) {
+      return svgElement
+    },
+    afterItem: function (svgElement) {
+      return svgElement
+    },
+    append: function ($element) {
+      $($element).append(
+        '<svg width="600" height="600" viewBox="0 0 600 600">' +
+        myChart.createChart.beforeItem() +
+        myChart.createChart.pathItem() +
+        myChart.createChart.afterItem() +
+        '</svg>')
+    }
+  }
 
-  console.log('k', k)
-  console.log('l', l)
-  console.log('m', m)
-
-  // $(document).ready(function () {
-  //   $('.test').append('<svg width="600" height="600" viewBox="0 0 600 600">' +
-  //   '<path id="test1" style="fill:#fe0; stroke:#fff; stroke-width:3;" />' +
-  //   '<path id="test2" style="fill:#fe0; stroke:#fff; stroke-width:3;" />' +
-  //   '<path id="test3" style="fill:#fe0; stroke:#fff; stroke-width:3;" />' +
-  //   '<path id="test4" style="fill:#fe0; stroke:#fff; stroke-width:3;" />' +
-  //   '<circle cx="100" cy ="100" r ="50" style="fill:#fff;"/>' +
-  //   '</svg>')
-  //   $('#test1').attr('d', 'M100 100,L' + k.starting_X + ' ' + k.starting_Y + 'A 100 100 0 0 0 ' + k.target_X + ' ' + k.target_Y + ',Z')
-  //   $('#test2').attr('d', 'M100 100,L' + l.starting_X + ' ' + l.starting_Y + 'A 100 100 0 1 0 ' + l.target_X + ' ' + l.target_Y + ',Z')
-  //   $('#test3').attr('d', 'M100 100,L' + m.starting_X + ' ' + m.starting_Y + 'A 100 100 0 0 0 ' + m.target_X + ' ' + m.target_Y + ',Z')
-  //   $('#test4').attr('d', 'M100 100,L' + n.starting_X + ' ' + n.starting_Y + 'A 100 100 0 0 0 ' + n.target_X + ' ' + n.target_Y + ',Z')
-  // })
+  $(document).ready(function () {
+    console.log(myChart)
+    myChart.createChart.afterItem = function () {
+      return '<circle cx ="100" cy ="100" r ="50" style ="fill:#fff";/>'
+    }
+    myChart.createChart.append('.test')
+  })
 })(jQuery)
