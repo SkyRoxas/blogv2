9435c2e7c1219d6a9d7885810f48a1354bae387b
jimmy8646 <yujiechueh@gmail.com>
2017-09-27 19:40:51 +0800

    add ga

--- header.php
+++ header.php
@@ -21,7 +21,16 @@
       enable_page_level_ads: true
     });
   </script>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-71309743-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/styles/css/style.css">
   <?php do_action('wp_head');?>
   <title><?php wp_title(); ?></title>
