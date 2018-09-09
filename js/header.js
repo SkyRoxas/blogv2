f02a54fa63880f4a3aee03a2fbbee2085af76408
jimmy8646 <yujiechueh@gmail.com>
2018-09-09 20:35:00 +0800

    fix:sticky header

--- js/header.js
+++ js/header.js
@@ -1,18 +1,19 @@
-// (function ($) {
-//   function headerScript () {
-//     $('#header').css('height', $('#header').children().innerHeight())
-//     $('#header').children().css({
-//       'position': 'fixed',
-//       'width': '100%',
-//       'z-index': '100'
-//     })
-//   }
+(function ($) {
+  function headerScript () {
+    $('#header').css('height', $('#header').children().innerHeight())
+    $('#header').children().css({
+      'position': 'fixed',
+      'top': 'auto',
+      'width': '100%',
+      'z-index': '100'
+    })
+  }
 
-//   $(document).ready(function () {
-//     headerScript()
-//   })
+  $(document).ready(function () {
+    headerScript()
+  })
 
-//   $(document).resize(function () {
-//     headerScript()
-//   })
-// })(jQuery)
+  $(document).resize(function () {
+    headerScript()
+  })
+})(jQuery)
